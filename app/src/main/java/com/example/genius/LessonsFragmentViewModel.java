d6fbd98bfb2902343db45ce736b0840cac08b43c
Ofek Elgozi <ofeke209@gmail.com>
2024-07-23 02:30:18 +0300

    Added Fragments/Activities

---
+++ app/src/main/java/com/example/genius/LessonsFragmentViewModel.java
@@ -0,0 +1,16 @@
+package com.example.genius;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LessonsFragmentViewModel extends ViewModel {
+
+
+}
