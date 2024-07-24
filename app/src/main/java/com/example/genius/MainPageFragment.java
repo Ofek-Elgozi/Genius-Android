a7654d4d98b0e0256bd289e111564c8443b5c757
Ofek Elgozi <ofeke209@gmail.com>
2024-07-24 22:53:43 +0300

    Started Main Page

--- app/src/main/java/com/example/genius/MainPageFragment.java
+++ app/src/main/java/com/example/genius/MainPageFragment.java
@@ -1,64 +1,56 @@
 package com.example.genius;
 
+import static com.example.genius.R.id.menu_profile;
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import com.example.genius.Model.Model;
+import com.example.genius.Model.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MainPageFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MainPageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainPageFragment newInstance(String param1, String param2) {
-        MainPageFragment fragment = new MainPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    User u;
+    String temp_email;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_page, container, false);
+        view = inflater.inflate(R.layout.fragment_main_page, container, false);
+        temp_email = MainPageFragmentArgs.fromBundle(getArguments()).getEmail();
+        Model.instance.getUserByEmail(temp_email, new Model.getUserByEmailListener() {
+            @Override
+            public void onComplete(User user) {
+                u = user;
+            }
+        });
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.toprofile_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if (itemID == R.id.menu_profile)
+        {
+            MainPageFragmentDirections.ActionMainPageFragmentToUserProfileFragment action = MainPageFragmentDirections.actionMainPageFragmentToUserProfileFragment(u);
+            Navigation.findNavController(view).navigate(action);
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/genius/UserProfileFragment.java
+++ app/src/main/java/com/example/genius/UserProfileFragment.java
@@ -2,19 +2,53 @@ package com.example.genius;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.example.genius.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class UserProfileFragment extends Fragment {
+    User user;
+    View view;
+
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        user = UserProfileFragmentArgs.fromBundle(getArguments()).getUser();
+        setHasOptionsMenu(true);
+        return view;
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.gethomepage_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if (itemID == R.id.home_menu)
+        {
+            Navigation.findNavController(view).popBackStack();
+        }
+        return true;
     }
 }
