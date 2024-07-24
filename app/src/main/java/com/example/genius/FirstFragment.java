202997bb8ce68dfbd48b462f1675a86518f453a8
Ofek Elgozi <ofeke209@gmail.com>
2024-07-24 04:06:55 +0300

    Created FirstFragment and connected it to Login page

--- app/src/main/java/com/example/genius/FirstFragment.java
+++ app/src/main/java/com/example/genius/FirstFragment.java
@@ -3,17 +3,43 @@ package com.example.genius;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view;
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Button StartBtn = view.findViewById(R.id.start_button);
+        StartBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (user != null)
+                {
+                    Toast.makeText(getActivity(), "Welcome To Genius!", Toast.LENGTH_SHORT).show();
+                    FirstFragmentDirections.ActionFirstFragmentToMainPageFragment action = FirstFragmentDirections.actionFirstFragmentToMainPageFragment(user.getEmail());
+                    Navigation.findNavController(v).navigate(action);
+                } else {
+                    Navigation.findNavController(v).navigate(R.id.loginFragment);
+                }
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/genius/MainActivity.java
+++ app/src/main/java/com/example/genius/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.genius;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event here
+            }
+        };
+
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
 }
