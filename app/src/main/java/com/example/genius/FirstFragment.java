efcfeab7f21baccac987a6e0128b16ab67be9725
Ofek Elgozi <ofeke209@gmail.com>
2024-07-24 18:36:00 +0300

    Fixed Register page.

--- app/src/main/java/com/example/genius/FirstFragment.java
+++ app/src/main/java/com/example/genius/FirstFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class FirstFragment extends Fragment {
 
     @Override
