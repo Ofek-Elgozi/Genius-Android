984f48446a28f7cf27efef960b76684e9c060f38
Ofek Elgozi <ofeke209@gmail.com>
2024-08-16 01:59:06 +0300

    Had some unneeded textview on main page

--- app/src/main/java/com/example/genius/Model/AppLocalDB.java
+++ app/src/main/java/com/example/genius/Model/AppLocalDB.java
@@ -5,7 +5,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = User.class, version =15)
+@Database(entities = User.class, version =16)
 abstract class AppLocalDbRepository extends RoomDatabase
 {
     public abstract UserDao userDao();
